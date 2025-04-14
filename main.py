c8ddd97faf8333c5bcfe5dfa54911ff51eb915a4
taikishank <124407665+taikishank@users.noreply.github.com>
2025-04-14 14:15:44 -0700

    Added main.py, schema.py, roles.txt
    Pseudocode for main and few schema instructions, roles.txt acts as reference for team members

---
+++ main.py
@@ -0,0 +1,12 @@
+'''
+Pseudocode
+
+scrape player data
+- normalize and clean data
+
+structure data into our JSON fields
+
+output data
+
+'''
+
---
+++ schema.py
@@ -0,0 +1,5 @@
+'''
+JSON schema for formatting
+refer to the README for all data we need for players
+'''
+
